b99bd96d3cbd3e618424fe6c8bd6878a9e2de4ff
apatelDF <40298114+apatelDF@users.noreply.github.com>
2018-06-15 10:11:56 -0500

    debug

--- ppLOGGER.py
+++ ppLOGGER.py
@@ -624,7 +624,7 @@ def updateSheets():
     vals =[]
     global prevLine
     prev =  prevLine
-    print(prev)
+    print(len(prev))
     log = open('log.log', 'rU')
     reader = csv.reader(log, delimiter=';')
     count = 0
