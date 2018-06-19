28ec311a7b25c0e90a998a9cfe675de860117941
apatelDF <40298114+apatelDF@users.noreply.github.com>
2018-06-19 11:26:07 -0500

    ts fix

--- ppLOGGER.py
+++ ppLOGGER.py
@@ -644,7 +644,9 @@ def updateSheets():
         # Add array of data at next open index of vals
         if (addToVals):
             prevLine = items[1:]
-            sensor_data['ts'] = int(datetime.now(tz=timezone.cst).timestamp() * 1000)
+            dts = datetime.datetime.utcnow()
+            epochtime = round(time.mktime(dts.timetuple()) + dts.milisecond/1e6)
+            sensor_data['ts'] = int(epochtime)
             sensor_data['values']['DIN1'] = items[1]
             sensor_data['values']['DIN2'] = items[2]
             sensor_data['values']['DIN3'] = items[3]
